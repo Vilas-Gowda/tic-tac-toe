864bafb6f570ae1c125e22fa6b9cbc6971d5fae6
Vilas-Gowda <vilasgowda65@gmail.com>
2022-07-30 10:12:09 +0530

    completed

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ clear = lambda: os.system('cls')
 clear()
 
 
-class tictacktoe:
+
 
     
 
-    def __init__(self):
-        print("---- game started ---")
+   
+print("---- game started ---")
         
     
 
-    def howto():
+def howto():
             print(''' 
 Player one plays X
 Player two plays O
 
-    0 1 2
-    3 4 5
-    6 7 8
+    1 2 3
+    4 5 6
+    7 8 9
             
 select respective place value to place your mark
 
@@ -29,8 +29,8 @@ select respective place value to place your mark
 
 
 
-    def printa(a):
-            for i in range(0,9,3):
+def printa(a):
+            for i in range(1,10,3):
                 for _ in range(i,i+3):
                     print("    ",end="")
                     print(a[_], end = "")
@@ -38,9 +38,9 @@ select respective place value to place your mark
     
     
     
-    while(True):
+while(True):
         
-        print('''
+    print('''
 TIC-TACK-TOE        
         
 1. Start
@@ -48,125 +48,114 @@ TIC-TACK-TOE
 3. Exit
             ''')
         
-        bi = int(input("Enter choice : "))
+    bi = int(input("Enter choice : "))
         
-        while(bi>3 or bi<1):
-            bi = int(input("Enter valid choice : "))
+    while(bi>3 or bi<1):
+        bi = int(input("Enter valid choice : "))
     
-        if(bi ==2):
-            clear()
-            howto()
-            print("Press enter to continue")
-            input()
-            clear()
+    if(bi ==2):
+        clear()
+        howto()
+        print("Press enter to continue")
+        input()
+        clear()
 
-        if(bi == 3):
-            clear()
-            print('''
+    if(bi == 3):
+        clear()
+        print('''
             
 ---- thanks for playing ----
             
             
-            ''')
-            exit()
+        ''')
+        exit()
         
-        if(bi == 1):
-            a = ['-' for _ in range(9)]
-            ipt = []
-            p1 = False
-            p2 = False
-            turn = 1
-            tries = 0
-            while(not p1 and not p2):
+    if(bi == 1):
+        a = ['-' for _ in range(10)]
+        ipt = []
+        p1 = False
+        p2 = False
+        turn = 1
+        tries = 0
+        while(True):
                 
-                for i1 in range(0,9,3):
-                    if(a[i1].upper == 'X' and a[i1+1].upper == 'X' and a[i1+2].upper == 'X'):
-                        p1 = True
-                    elif (a[i1].upper == 'O' and a[i1+1].upper == 'O' and a[i1+2].upper == 'O'):
-                        p2 = True
-                
-                for i1 in range(0,3):
-                    if(a[i1].upper == 'X' and a[i1+3].upper == 'X' and a[i1+6].upper == 'X'):
-                        p1 = True
-                    elif (a[i1].upper == 'O' and a[i1+3].upper == 'O' and a[i1+6].upper == 'O'):
-                        p2 = True
-
-                if(a[2].upper == 'X' and a[4].upper == 'X' and a[6].upper == 'X'):
+            for i1 in range(1,10,3):
+                if(a[i1] == 'X' and a[i1+1] == 'X' and a[i1+2] == 'X'):
                     p1 = True
-                elif (a[2].upper == 'O' and a[4].upper == 'O' and a[6].upper == 'O'):
+                elif (a[i1] == 'O' and a[i1+1] == 'O' and a[i1+2] == 'O'):
                     p2 = True
-
-                if(a[0].upper == 'X' and a[4].upper == 'X' and a[8].upper == 'X'):
-                    p1 = True
-                elif (a[0].upper == 'O' and a[4].upper == 'O' and a[8].upper == 'O'):
+                    
+                
+            for i1 in range(1,4):
+                if(a[i1] == 'X' and a[i1+3] == 'X' and a[i1+6] == 'X'):
+                    p1 = True   
+                elif (a[i1] == 'O' and a[i1+3] == 'O' and a[i1+6] == 'O'):
                     p2 = True
+                    
 
-                clear()
-                print("\n\n")
+            if(a[2] == 'X' and a[4] == 'X' and a[6] == 'X'):
+                p1 = True  
+            elif (a[2] == 'O' and a[4] == 'O' and a[6] == 'O'):
+                p2 = True
+                
 
-                printa(a)
-                print('\n\n')
-                print(f"Player {turn}'s turn, enter index")
+            if(a[0] == 'X' and a[4] == 'X' and a[8] == 'X'):
+                p1 = True   
+            elif (a[0] == 'O' and a[4] == 'O' and a[8] == 'O'):
+                p2 = True
                 
-                index = int(input())
-                
-                
-                while(index>8 and index<0):
-                    index = int(input("Enter correct index : "))
-                
-                while(index in ipt):
-                    index = int(input("Enter another index : "))
 
-                ipt.append(index)
-                
-                if(turn==1):
-                    a[index] = 'X'
-                elif(turn==2):
-                    a[index] = 'O'
-                
-                if(turn==1):
-                    turn=2
-                elif(turn==2):
-                    turn=1
-                tries += 1
-                if(tries ==8):
-                    break
-            
-            if(p1):
-                clear()
-                print("\n\n----Player 1 WINS!!----")
-                print("\n\nPress enter to continue")
-                input()
-            if(p2):
-                clear()
-                print("\n\n----Player 2 WINS!!----")
-                print("\n\nPress enter to continue")
-                input()
-            if(tries == 9):
-                clear()
-                print("\n\n----DRAW----")
-                print("\n\nPress enter to continue")
-                input()
-            
-            
+            if(p1 or p2):
+                break
             clear()
+            print("\n\n")
+            print(a)
             
 
-    
+            printa(a)
+            print('\n\n')
+            print(f"Player {turn}'s turn, enter index")
+                
+            index = int(input())
+                
+                
+            while(index>9 or index<0):
+                index = int(input("Enter correct index : "))
+                
+            while(index in ipt):
+                index = int(input("Enter another index : "))
 
-gamestart = tictacktoe()
-
-        
-
-
-        
-        
-        
-
-        
-
+            ipt.append(index)
+                
+            if(turn==1):
+                a[index] = 'X'
+            elif(turn==2):
+                a[index] = 'O'
+                
+            if(turn==1):
+                turn=2
+            elif(turn==2):
+                turn=1
+            tries += 1
+            print(tries)
+            if(tries==9):
+                break
             
-  
-        
-
-    
+        if(p1):
+            clear()
+            print("\n\n----Player 1 WINS!!----")
+            print("\n\nPress enter to continue")
+            input()
+        if(p2):
+            clear()
+            print("\n\n----Player 2 WINS!!----")
+            print("\n\nPress enter to continue")
+            input()
+        if(tries == 8):
+            clear()
+            print("\n\n----DRAW----")
+            print("\n\nPress enter to continue")
+            input()
+            
+            
+        clear()
